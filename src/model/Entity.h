951247ed8f86c25069a83e9d4643e880417f0c89
Abrikoos <45069148+Triple1312@users.noreply.github.com>
2020-12-12 00:31:42 +0100

    Update Entity.h

--- src/model/Entity.h
+++ src/model/Entity.h
@@ -12,29 +12,52 @@
 #include <glm/gtx/transform.hpp>
 #include "model/Clock.hpp"
 
+/**
+*@brief: an enum that describes at which axis two entities collide;
+*/
 enum collission_type {no_col = false, x_col = true, y_col = true, z_col = true };
 
 namespace logic {
 
 class Entity {
 public:
-
+    /**
+    * @brief: describes the velocity in every axis
+    */
     glm::vec3 velocity = glm::vec3(0.0f, 0.0f, 0.0f);
 
+
+    /**
+    *@brief: saves every point of the entity
+    * an entity will always be a recengular cube
+    */
     float points[24]; // stride is 3
 
 
-
+    /**
+    * @brief checks wich collision is present between 2 etities if any
+    */
     virtual collission_type Collision(std::shared_ptr<Entity>);
 
+    /**
+    *@brief returns a pointer to a c-array whinch holds all the points of the entiry
+    */
     void *GetPoints(); //returns a pointer to an array of 24 floats
 
+    /**
+    *@brief returns the center point of the entity
+    */
     virtual glm::vec4 GetCenter();
-
+    
+    /**
+    *@brief moves the entity to a position with as position the given point
+    *@idk the center position used to set the position of every point
+    */
     virtual void SetPosition(glm::vec3);
 
     virtual void SetPosition(glm::vec3 pos, glm::vec3 size);
 
+     
     virtual glm::vec3 GetPosition();
 
     virtual void SetPosNoX(float y, float z);
